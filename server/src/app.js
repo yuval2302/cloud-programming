7124dd019fe255913980b22cb663072a33251fba
ohad <ohadah1@gmail.com>
2020-02-07 17:39:49 +0200

    add body-parser

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 var mysql = require('mysql');
 
@@ -10,7 +30,14 @@ var con = mysql.createConnection({
     password: "yourpassword"
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!')
+    console.log("ohad");
+    res.status(200).send("hello")
+});
 
 app.get('/order', (req, res) => {
     let sql = "SELECT * from orders;";
