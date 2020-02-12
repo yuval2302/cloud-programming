56ac140bd32ff18871b58f5c7f3c0994a9a9f583
ohad <ohadah1@gmail.com>
2020-02-12 14:28:03 +0200

    add products query to server

--- server/src/app.js
+++ server/src/app.js
@@ -30,7 +30,7 @@ var con = mysql.createConnection({
     password: "Aa123456",
 });
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -44,6 +44,7 @@ app.get('/order', (req, res) => {
     mysql.createQuery(sql, (val) => {
         res.send(val)
     });
+    res.send("hello");
 });
 
 app.post('/order', (req, res) => {
@@ -55,7 +56,40 @@ app.post('/order', (req, res) => {
 
 app.get('/product', (req, res) => {
     let sql = "SELECT * from product;";
-    res.send('Hello World!')
+    let products = [
+        {
+            'id': '9aa113b4-1e4e-4cde-bf9d-8358fc78ea4f',
+            'price': 3.50,
+            'name': 'Greens',
+            'description': 'Looking for simple, clean and green? Four of the most nutrient dense leafy greens create the base in our low-sugar Greens 1.'
+        },
+        {
+            'id': 'bdcbe438-ac85-4acf-8949-5627fd5b57df',
+            'price': 2.75,
+            'name': 'Citrus',
+            'description': 'This enzyme rich juice is filled with phytonutrients and bromelin which helps to reduce inflammation. Drink it before a meal to get digestive juices flowing.'
+        },
+        {
+            'id': '58552daa-30f6-46fa-a808-f1a1d7667561',
+            'price': 3,
+            'name': 'Roots',
+            'description': 'Beets help your body to release stomach acid which aids digestion! Drink this juice when you want a snack that\'s both pretty and nutritious!'
+        },
+        {
+            'id': 'd4666802-fd84-476f-9eea-c8dd29cfb633',
+            'price': 1.99,
+            'name': 'Orange',
+            'description': 'Orange juice with a twist to boost you health!'
+        },
+        {
+            'id': '7ef3b9dd-5a95-4415-af37-6871d6ff0262',
+            'price': 2.50,
+            'name': 'Coconut',
+            'description': 'Cinnamon lovers - this is your blend! Two nutritional powerhouses combine in a delicious, satiating elixir. Both cinnamon and coconut have been shown to reduce blood sugar. Raw coconut meat is a great source of medium chain fatty acids, which can lower bad cholesterol. Coconut also contains significant levels of fiber and manganese, a mineral that helps you metabolize fat and protein.'
+        }
+    ];
+
+    res.send(products);
 });
 
 app.post('/product', (req, res) => {
