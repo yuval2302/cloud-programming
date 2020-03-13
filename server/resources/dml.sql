e815b5500ab43cb602c9e217216d9a353bdb1a4c
ohad <ohadah1@gmail.com>
2020-03-13 18:58:20 +0200

    ymls and db script

--- server/resources/dml.sql
+++ server/resources/dml.sql
@@ -1,29 +1,29 @@
-insert into PRODUCT(NAME,DESCRIPTION, PRICE) values(
+insert into PRODUCTS(NAME,DESCRIPTION, PRICE) values(
     'Greens',
     'Looking for simple, clean and green? Four of the most nutrient dense leafy greens create the base in our low-sugar Greens 1.',
      3.50
 );
 
-insert into PRODUCT(NAME,DESCRIPTION, PRICE) values(
+insert into PRODUCTS(NAME,DESCRIPTION, PRICE) values(
     'Citrus',
     'This enzyme rich juice is filled with phytonutrients and bromelin which helps to reduce inflammation. Drink it before a meal to get digestive juices flowing.',
      2.75
 );
 
-insert into PRODUCT(NAME,DESCRIPTION, PRICE) values(
+insert into PRODUCTS(NAME,DESCRIPTION, PRICE) values(
      'Roots',
      "Beets help your body to release stomach acid which aids digestion! Drink this juice when you want a snack that's both pretty and nutritious!",
      3
 );
 
-insert into PRODUCT(NAME,DESCRIPTION, PRICE) values(
+insert into PRODUCTS(NAME,DESCRIPTION, PRICE) values(
      'Orange',
      'Orange juice with a twist to boost you health!',
-     1.99,
+     1.99
 );
 
-insert into PRODUCT(NAME,DESCRIPTION, PRICE) values(
+insert into PRODUCTS(NAME,DESCRIPTION, PRICE) values(
     'Coconut',
     'Cinnamon lovers - this is your blend! Two nutritional powerhouses combine in a delicious, satiating elixir. Both cinnamon and coconut have been shown to reduce blood sugar. Raw coconut meat is a great source of medium chain fatty acids, which can lower bad cholesterol. Coconut also contains significant levels of fiber and manganese, a mineral that helps you metabolize fat and protein.',
-    2.50,
+    2.50
 );
